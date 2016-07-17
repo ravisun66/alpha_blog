71254c58d94ae3d781361799a376e92ea0da1138
Ravi Shankar Jha <ravi.sun66@gmail.com>
2016-07-17 21:24:33 +0530

    create flash messages and create show action

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = "Article was successfully created."
+			redirect_to article_path(@article)
+		else
+			flash[:notice] = "Some validations doesn't let it to be saved."
+			render 'new'
+		end
+	end
+
+	def show
+		@article = Article.find(params[:id])
+
+	end
+	private
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+end
